439421c55e745de9b3284c47ffeae309da3be60f
hoon <stitch8971@gachon.ac.kr>
2026-07-19 18:34:00 +0900

    feat: 스프링 MVC 초기 설정 및 테스트 API 추가

--- src/main/java/com/avocado/config/RootConfig.java
+++ src/main/java/com/avocado/config/RootConfig.java
@@ -1,4 +1,7 @@
 package com.avocado.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RootConfig {
 }
--- src/main/java/com/avocado/config/ServletConfig.java
+++ src/main/java/com/avocado/config/ServletConfig.java
@@ -1,4 +1,13 @@
 package com.avocado.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "com.avocado.home.controller"
+})
 public class ServletConfig {
 }
