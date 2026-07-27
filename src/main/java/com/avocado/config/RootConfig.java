d5c6c5ab7b5e41a04b014248dade36d9a52debe0
hoon <stitch8971@gachon.ac.kr>
2026-07-28 02:28:54 +0900

    feat: MySQL 데이터베이스 연결 설정 추가

--- src/main/java/com/avocado/config/RootConfig.java
+++ src/main/java/com/avocado/config/RootConfig.java
@@ -29,6 +29,10 @@ public class RootConfig {
     @Value("${db.password}")
     private String password;
 
+    /**
+     * MySQL 연결에 사용할 HikariCP DataSource를 생성.
+     * @return 구현체 HikariDataSource
+     */
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
@@ -38,9 +42,13 @@ public class RootConfig {
         config.setUsername(username);
         config.setPassword(password);
 
+        // 커넥션 풀 식별 이름
         config.setPoolName("AvocadoHikariPool");
+        // 풀에 들어가는 최대 커넥션 수
         config.setMaximumPoolSize(10);
+        // 유후 상태 커넥션 수
         config.setMinimumIdle(2);
+        // 커넥션 획득 대기 최대 30초
         config.setConnectionTimeout(30_000);
 
         return new HikariDataSource(config);
