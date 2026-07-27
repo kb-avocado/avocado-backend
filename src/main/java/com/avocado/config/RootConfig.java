ffde0ccc05909262aa1288f6447ae6f473cdf182
hoon <stitch8971@gachon.ac.kr>
2026-07-28 02:00:05 +0900

    feat: MySQL 데이터베이스 연결 설정 추가

--- src/main/java/com/avocado/config/RootConfig.java
+++ src/main/java/com/avocado/config/RootConfig.java
@@ -1,7 +1,48 @@
 package com.avocado.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource(
+        value = {
+                "classpath:config/application-local.properties"
+        }
+)
 public class RootConfig {
+
+    @Value("${db.driver}")
+    private String driverClassName;
+
+    @Value("${db.url}")
+    private String jdbcUrl;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName(driverClassName);
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        config.setPoolName("AvocadoHikariPool");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30_000);
+
+        return new HikariDataSource(config);
+    }
 }
