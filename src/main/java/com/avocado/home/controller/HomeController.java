9ee3c7ab09f83ba378b264f50736160905e53342
hoon <stitch8971@gachon.ac.kr>
2026-07-29 00:21:53 +0900

    [AVCD-23] test: Swagger 확인용 API 추가

--- src/main/java/com/avocado/home/controller/HomeController.java
+++ src/main/java/com/avocado/home/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.avocado.home.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
-        return "Hello World";
+    public ResponseEntity<Map<String, String>> home() {
+        return ResponseEntity.ok(
+                Map.of("status", "up")
+        );
     }
+
 }
