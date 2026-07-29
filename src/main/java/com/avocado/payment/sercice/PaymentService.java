4cbc4b8bba55382494e3cbdabe1a68dae4aa3ca1
hoon <stitch8971@gachon.ac.kr>
2026-07-29 10:14:45 +0900

    chore: 초기 패키지 구조 설정

---
+++ src/main/java/com/avocado/payment/sercice/PaymentService.java
@@ -0,0 +1,7 @@
+package com.avocado.payment.sercice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+}
---
+++ src/main/java/com/avocado/wallet/service/WalletService.java
@@ -0,0 +1,7 @@
+package com.avocado.wallet.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+}
