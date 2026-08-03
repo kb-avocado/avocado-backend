b1c39f3ba8f4ad7cafd88bfd27b0ab779dacee1c
hoon <stitch8971@gachon.ac.kr>
2026-08-03 13:12:25 +0900

    fix: 공통 API 응답 수정

--- src/main/java/com/avocado/common/response/ApiResponse.java
+++ src/main/java/com/avocado/common/response/ApiResponse.java
@@ -10,13 +10,25 @@ import lombok.Getter;
 @ApiModel(description = "API 공통 응답 포맷")
 public class ApiResponse<T> {
 
-    @ApiModelProperty(value = "성공 여부", example = "true", required = true)
+    @ApiModelProperty(
+            value = "성공 여부",
+            example = "true",
+            required = true
+    )
     private final boolean success;
 
-    @ApiModelProperty(value = "응답 코드", example = "SUCCESS", required = true)
+    @ApiModelProperty(
+            value = "응답 코드",
+            example = "SUCCESS",
+            required = true
+    )
     private final String code;
 
-    @ApiModelProperty(value = "응답 메시지", example = "요청이 성공적으로 처리되었습니다.", required = true)
+    @ApiModelProperty(
+            value = "응답 메시지",
+            example = "요청이 성공적으로 처리되었습니다.",
+            required = true
+    )
     private final String message;
 
     @ApiModelProperty(value = "응답 데이터 (없을 시 NULL)")
