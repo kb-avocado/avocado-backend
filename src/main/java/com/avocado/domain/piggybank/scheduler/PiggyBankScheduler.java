41933c2efcb95a9b6ef03782a115560db0060fa2
rlaghwjd2402-ctrl <rlaghwjd2402@gmail.com>
2026-08-10 15:54:58 +0900

    [AVCD-116] feat: 저금통 7일 달성 승격 스케줄러 추가

--- src/main/java/com/avocado/domain/piggybank/scheduler/PiggyBankScheduler.java
+++ src/main/java/com/avocado/domain/piggybank/scheduler/PiggyBankScheduler.java
@@ -1,4 +1,20 @@
 package com.avocado.domain.piggybank.scheduler;
 
+import com.avocado.domain.piggybank.service.PiggyBankService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+// 저금통 7일 달성 승격 스케줄러
+@Component
+@RequiredArgsConstructor
 public class PiggyBankScheduler {
+
+    private final PiggyBankService piggyBankService;
+
+    // 매일 자정 실행 (초 분 시 일 월 요일)
+    @Scheduled(cron = "0 0 0 * * *")
+    public void promoteAchievements() {
+        piggyBankService.promoteAchievements();
+    }
 }
