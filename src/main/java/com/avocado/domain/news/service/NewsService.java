8e6edcb0800ef0e115b841edc7e1c6545536edc3
hisemsem.github.io <121273265+hisemsem@users.noreply.github.com>
2026-08-16 04:11:45 +0900

    [AVCD-165] feat: 전체적인 UI/UX 수정

--- src/main/java/com/avocado/domain/news/service/NewsService.java
+++ src/main/java/com/avocado/domain/news/service/NewsService.java
@@ -8,7 +8,7 @@ import com.avocado.domain.news.dto.response.NewsListResponseDto;
 import com.avocado.global.security.jwt.dto.AuthUser;
 
 public interface NewsService {
-    NewsListResponseDto getNewsList(int page, int size, Long childId, AuthUser authUser);
+    NewsListResponseDto getNewsList(int page, int size, Long childId,Boolean completed,  AuthUser authUser);
     NewsDetailResponseDto getNewsDetail(Long newsId, Long childId, AuthUser authUser);
     NewsAnswerResponseDto saveAnswer(Long newsId, Long childId, AuthUser authUser, NewsAnswerRequestDto request);
 }
